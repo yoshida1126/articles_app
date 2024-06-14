bf4b9dca299679b5faf5f58d4cfd58411c486b01
yoshida1126’; GIT_AUTHOR_EMAIL=wwstnugzvctc6qpg1i1s@docomo.ne.jp; GIT_COMMITTER_NAME=yoshida1126’ <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2024-06-14 13:34:25 +0900

    add a static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /help" do
-    it "returns http success" do
-      get "/static_pages/help"
-      expect(response).to have_http_status(:success)
+  describe "#help" do
+    it "正常にレスポンスを返すこと" do
+      get static_pages_help_path
+      expect(response).to have_http_status :ok
     end
   end
 
-  describe "GET /about" do
-    it "returns http success" do
-      get "/static_pages/about"
-      expect(response).to have_http_status(:success)
+  describe "#about" do
+    it "正常にレスポンスを返すこと" do
+      get static_pages_about_path
+      expect(response).to have_http_status :ok 
     end
   end
 
