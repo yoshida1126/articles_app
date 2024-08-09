c19c1738f73656dc60741519582000c36f51b1be
yoshida1126’; GIT_AUTHOR_EMAIL=wwstnugzvctc6qpg1i1s@docomo.ne.jp; GIT_COMMITTER_NAME=yoshida1126’ <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2024-08-09 17:45:32 +0900

    update a production setting

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,3 @@ bundle install
 bundle exec rails assets:precompile 
 bundle exec rails assets:clean 
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
-bundle exec rails db:seed
