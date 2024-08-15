d2e8f68e97495c4f5fee2c5166104248d10b71eb
yoshida1126’; GIT_AUTHOR_EMAIL=wwstnugzvctc6qpg1i1s@docomo.ne.jp; GIT_COMMITTER_NAME=yoshida1126’ <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2024-08-15 17:31:25 +0900

    add a rack-cors-gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,10 +90,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Request-Methods' => '*',
-    'Access-Control-Request-Headers' => '*'
-  }
 end
