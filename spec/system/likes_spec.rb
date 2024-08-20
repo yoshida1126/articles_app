da0460772d63b6be8f4e78750f8dc31cabf31749
yoshida1126’; GIT_AUTHOR_EMAIL=wwstnugzvctc6qpg1i1s@docomo.ne.jp; GIT_COMMITTER_NAME=yoshida1126’ <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2024-08-20 14:44:57 +0900

    add user-following-tests

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper' 
 
 RSpec.describe "Likes", type: :system do 
-    before do 
-        driven_by(:rack_test)
-    end 
+  before do 
+    driven_by(:rack_test)
+  end 
 end 
 
 describe "#create" do 
