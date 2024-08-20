cea3a6568450afd98b4a159d28d6c8b916aa3eac
yoshida1126’; GIT_AUTHOR_EMAIL=wwstnugzvctc6qpg1i1s@docomo.ne.jp; GIT_COMMITTER_NAME=yoshida1126’ <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2024-08-20 15:06:47 +0900

    update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 # exit on error 
 set -o errexit 
-bundle _2.4.12_ install 
+bundle install 
 bundle exec rails assets:precompile 
 bundle exec rails assets:clean 
 bundle exec rails db:migrate 
