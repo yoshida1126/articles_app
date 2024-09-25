8543f3c4efdec89719d32b566986193e2c0ba69a
yoshida1126’; GIT_AUTHOR_EMAIL=wwstnugzvctc6qpg1i1s@docomo.ne.jp; GIT_COMMITTER_NAME=yoshida1126’ <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2024-09-25 12:38:52 +0900

    change after-update-path

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -78,6 +78,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end 
   end 
 
+  def after_update_path_for(resource) 
+    user_path(resource) 
+  end 
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
