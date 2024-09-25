f6ec519cd4e06e36a8ff228ec7be8f99b2ec4768
yoshida1126’; GIT_AUTHOR_EMAIL=wwstnugzvctc6qpg1i1s@docomo.ne.jp; GIT_COMMITTER_NAME=yoshida1126’ <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2024-09-25 12:11:59 +0900

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module ArticlesApp
 
     config.active_record.default_timezone = :local
 
+    Faker::Config.locale = :en 
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
