d9e9fc0addb90e1d2abf7ba4252bb5acb408f7cf
yoshida1126’; GIT_AUTHOR_EMAIL=wwstnugzvctc6qpg1i1s@docomo.ne.jp; GIT_COMMITTER_NAME=yoshida1126’ <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2024-11-20 20:59:35 +0900

    deploy setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ ArticlesApp::Application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "articles_app_production"
 
-  config.action_mailer.default_url_options = { host: '3.112.62.12', port: 3000 }
+  config.action_mailer.default_url_options = { host: '3.112.62.12' }
 
   config.action_mailer.raise_delivery_errors = true
 
