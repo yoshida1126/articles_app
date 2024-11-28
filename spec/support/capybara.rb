4143250bf731b59be1684cd60467374c3b3b0a8b
yoshida1126’; GIT_AUTHOR_EMAIL=wwstnugzvctc6qpg1i1s@docomo.ne.jp; GIT_COMMITTER_NAME=yoshida1126’ <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2024-11-28 15:42:23 +0900

    edit capybara.rb

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -23,7 +23,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
       driven_by :selenium, using: :chrome, options: {
         browser: :remote,
-        url: ENV.fetch('SELENIUM_DRIVER_URL')
+        url: "http://chrome:4444/wd/hub"
       }
       Capybara.server_host = 'app'
   end
