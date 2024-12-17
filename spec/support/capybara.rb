77e90111b1dc3a8d07b62ab13482e2118cf91276
yoshida1126’; GIT_AUTHOR_EMAIL=wwstnugzvctc6qpg1i1s@docomo.ne.jp; GIT_COMMITTER_NAME=yoshida1126’ <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2024-12-17 12:37:03 +0900

    circleci setting

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -25,6 +25,6 @@ RSpec.configure do |config|
         browser: :remote,
         url: "http://chrome:4444/wd/hub"
       }
-      Capybara.server_host = '127.0.0.0:3000'
+      Capybara.server_host = 'localhost:3000'
   end
 end
