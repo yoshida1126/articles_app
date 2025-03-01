ab3b3b1edfd46ca9437eea0e551e22a35b762a95
yoshida1126 <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2025-03-01 11:53:11 +0900

    update-menu.js

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -2,15 +2,16 @@ document.addEventListener("turbo:load", menu);
 document.addEventListener("turbo:render", menu);
 
 function menu() {
-  document.removeEventListener("turbo:load", menu);
+  if(document.getElementById('dropdown-menu')){
+    document.removeEventListener("turbo:load", menu);
     let account = document.querySelector("#account");
     if (account) {
-        account.addEventListener("click", function(event) { 
-          event.preventDefault();
-          let menu = document.querySelector("#dropdown-menu");
-          menu.classList.toggle("active");
-          event.stopPropagation();
-        });
+      account.addEventListener("click", function(event) { 
+        event.preventDefault();
+        let menu = document.querySelector("#dropdown-menu");
+        menu.classList.toggle("active");
+        event.stopPropagation();
+      });
     }
 
     let active = document.getElementsByClassName("active");
@@ -18,12 +19,16 @@ function menu() {
     if (active) {
       $('html').click(function(e) {
         if(!$(e.target).closest('.dropdown-menu').length) {
-          let menu = document.querySelector("#dropdown-menu");
-          menu.classList.remove("active");
+          if(document.getElementById('dropdown-menu')){
+            let menu = document.querySelector("#dropdown-menu");
+            menu.classList.remove("active");
+          }
         }
       });
     }
+  }
 
+  if(document.getElementById('dropdown-menu3')){
     let account2 = document.querySelector("#account2");
     if (account2) {
         account2.addEventListener("click", function(event) {
@@ -34,15 +39,21 @@ function menu() {
         });
     }
 
+    let active = document.getElementsByClassName("active");
+
     if(active) {
       $('html').click(function(e) {
         if(!$(e.target).closest('.responsive-header').length) {
-          let menu = document.querySelector("#dropdown-menu3");
-          menu.classList.remove("active");
+          if(document.getElementById('dropdown-menu3')){
+            let menu = document.querySelector("#dropdown-menu3");
+            menu.classList.remove("active");
+          }
         }
       });
     }
+  }
 
+  if(document.getElementById('dropdown-menu2')){
     if (document.querySelector("#dropdown-menu2")) {
       let option = document.querySelector("#option");
       if (option) {
@@ -53,6 +64,8 @@ function menu() {
             event.stopPropagation();
           });
       }
+
+      let active = document.getElementsByClassName("active");
 
       if(active) {
         let option = document.querySelector(".article-option");
@@ -70,4 +83,5 @@ function menu() {
         }
       }
     }
+  }
 };
