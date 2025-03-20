f15e5b590953bc64e3f673e331f5b2e2881c614f
yoshida1126 <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2025-03-20 14:29:58 +0900

    update comment-count

--- app/javascript/custom/count.js
+++ app/javascript/custom/count.js
@@ -1,22 +1,22 @@
-if(document.getElementsByClassName("count").length) {
+if(document.getElementById("count_text")) {
     document.addEventListener("turbo:load", count);
-    document.addEventListener("turbo:render", count);
+    document.addEventListener("turbo:render", count); 
+}
 
-    function count() {  
-        let count = document.getElementsByClassName("count");
-        const articleText = document.getElementById('markdown');
-        articleText.addEventListener("keyup", () => {
-            let length = articleText.value.length
-            let countText = document.getElementById('count_text')
-            if(length == 3000) {
-                countText.classList.add("limit-length")
+function count() {  
+    let count = document.getElementsByClassName("count");
+    const articleText = document.getElementById('markdown');
+    articleText.addEventListener("keyup", () => {
+        let length = articleText.value.length
+        let countText = document.getElementById('count_text')
+        if(length == 3000) {
+            countText.classList.add("limit-length")
+        }
+        if(length < 3000) {
+            if(document.getElementsByClassName("limit-length")) {
+              countText.classList.remove("limit-length")
             }
-            if(length < 3000) {
-                if(document.getElementsByClassName("limit-length")) {
-                  countText.classList.remove("limit-length")
-                }
-                countText.innerHTML = `${length} ／3000文字`
-            }
-        });
-    }
+            countText.innerHTML = `${length} ／3000文字`
+        }
+    });
 }
