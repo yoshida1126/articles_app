f962256f3296e4492adff3a76b1c5b0fc98d5d43
yoshida1126 <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2025-03-21 17:45:30 +0900

    edit unicorn.conf.rb

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -21,8 +21,7 @@ before_fork do |server, worker|
   if old_pid != server.pid
     begin
       Process.kill "QUIT", File.read(old_pid).to_i
-    rescue
-      Errno::ENOENT, Errno::ESRCH
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
 end
