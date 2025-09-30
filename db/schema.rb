1519549c80f74edd844e59350c59a3b579dbc1d6
yoshida1126 <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2025-09-30 16:11:47 +0900

    add admin column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_07_28_024351) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_30_070810) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -159,6 +159,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_28_024351) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "introduction"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
