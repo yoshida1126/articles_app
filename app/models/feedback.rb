ea5e457d7589a32ed7a5d7cbf9d46d1e1d0e1ec5
yoshida1126 <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2025-10-03 13:31:57 +0900

    Described feedback model validation

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,9 @@
 class Feedback < ApplicationRecord
     belongs_to :user
+
+    validates :subject, presence: true,
+                        length: { minimum: 2, maximum: 50, allow_blank: true }
+    
+    validates :body, presence: true,
+                     length: { minimum: 2, maximum: 1500, allow_blank: true }
 end
