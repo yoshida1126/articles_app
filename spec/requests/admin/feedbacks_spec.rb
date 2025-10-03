f71f2c0dcc9f7b931420c23ce27ff3e9f3bbac6e
yoshida1126 <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2025-10-03 12:58:01 +0900

    Fix for testing feedback feature

--- spec/requests/admin/feedbacks_spec.rb
+++ spec/requests/admin/feedbacks_spec.rb
@@ -2,17 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Admin::Feedbacks", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/admin/feedbacks/index"
-      expect(response).to have_http_status(:success)
-    end
   end
 
   describe "GET /destroy" do
-    it "returns http success" do
-      get "/admin/feedbacks/destroy"
-      expect(response).to have_http_status(:success)
-    end
   end
 
 end
--- spec/requests/feedbacks_spec.rb
+++ spec/requests/feedbacks_spec.rb
@@ -2,17 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Feedbacks", type: :request do
   describe "GET /new" do
-    it "returns http success" do
-      get "/feedbacks/new"
-      expect(response).to have_http_status(:success)
-    end
   end
 
   describe "GET /create" do
-    it "returns http success" do
-      get "/feedbacks/create"
-      expect(response).to have_http_status(:success)
-    end
   end
 
 end
