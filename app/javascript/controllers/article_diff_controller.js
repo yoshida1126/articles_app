0d99123d4f609857d49f8f0b0e8bd06091d05938
yoshida1126 <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2025-11-12 21:26:31 +0900

    Fix bug in diff view when original text lacks trailing newline

--- app/javascript/controllers/article_diff_controller.js
+++ app/javascript/controllers/article_diff_controller.js
@@ -24,77 +24,28 @@ export default class extends Controller {
   diff() {
     const normalize = str => str.replace(/\r\n/g, '\n').replace(/\r/g, '\n')
 
-    const original = normalize(JSON.parse(this.originalTarget.textContent))
-    const current = normalize(this.textarea.value)
+    const original = normalize(JSON.parse(this.originalTarget.textContent) + '\n')
+    const current = normalize(this.textarea.value + '\n')
 
     const diffs = diffLines(original, current)
 
     this.outputTarget.innerHTML = ''
 
-    let lastAddedDiv = null;
-    let previousText = null;
-
     diffs.forEach(part => {
       const lines = part.value.split(/(?<=\n)/)
 
-      lines.forEach((line, i) => {
+      lines.forEach(line => {
         const div = document.createElement('div')
         div.classList.add('diff-line')
 
         const escapedLine = this.escapeHTML(line)
 
         if (part.added) {
-          if (lastAddedDiv) {
-            if (i === 0) {
-              if (previousText + '\n' === line) {
-                lastAddedDiv.textContent = escapedLine
-                this.outputTarget.appendChild(lastAddedDiv)
-              } else {
-                lastAddedDiv.innerHTML = `- ${previousText}`
-                lastAddedDiv.classList.add('removed')
-                this.outputTarget.appendChild(lastAddedDiv)
-
-                div.innerHTML = `+ ${escapedLine}`
-                div.classList.add('added')
-                this.outputTarget.appendChild(div)
-              }
-              lastAddedDiv = document.createElement('div')
-              lastAddedDiv.classList.add('diff-line')
-              lastAddedDiv.classList.add('added')
-
-              if (line.includes('\n')) {
-                lastAddedDiv.textContent = '+'
-                this.outputTarget.appendChild(lastAddedDiv)
-              }
-            } else {
-              lastAddedDiv.textContent = `+ ${escapedLine}`
-              this.outputTarget.appendChild(lastAddedDiv)
-
-              lastAddedDiv = document.createElement('div')
-              lastAddedDiv.classList.add('diff-line')
-              lastAddedDiv.classList.add('added')
-
-              if (line.includes('\n')) {
-                lastAddedDiv.textContent = '+'
-                this.outputTarget.appendChild(lastAddedDiv)
-              }
-            }
-            return
-          } else {
-            div.innerHTML = `+ ${escapedLine}`
-            div.classList.add('added')
-          }
+          div.innerHTML = `+ ${escapedLine}`
+          div.classList.add('added')
         } else if (part.removed) {
-          if (!(line.endsWith('\n'))) {
-            previousText = escapedLine
-
-            lastAddedDiv = document.createElement('div')
-            lastAddedDiv.classList.add('diff-line')
-            return
-          } else {
-            div.innerHTML = `- ${escapedLine}`
-            div.classList.add('removed')
-          }
+          div.innerHTML = `- ${escapedLine}`
+          div.classList.add('removed')
         } else {
           div.innerHTML = `${escapedLine}`
         }
