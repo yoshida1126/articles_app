6b5a4ef0eaf4a20f3d48cbf7d7b1afd4debef23c
yoshida1126 <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2025-11-24 19:31:06 +0900

    Fix mobile layout issues in file size bar and adjust visual styling

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -9,6 +9,27 @@ export default class extends Controller {
   static targets = ["quota", "bar"]
 
   connect() {
-    updateSizeBar(this.quotaTarget, this.barTarget, this.remainingValue, this.maxValue)
+    updateSizeBar(
+      this.quotaTarget,
+      this.barTarget,
+      this.remainingValue,
+      this.maxValue
+    )
+
+    this.mediaQuery = window.matchMedia("(max-width: 640px)")
+    this._mqCallback = () => {
+      updateSizeBar(
+        this.quotaTarget,
+        this.barTarget,
+        this.remainingValue,
+        this.maxValue
+      )
+    }
+
+    this.mediaQuery.addEventListener("change", this._mqCallback)
+  }
+
+  disconnect() {
+    this.mediaQuery?.removeEventListener("change", this._mqCallback)
   }
 }
